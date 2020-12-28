fd233647e0916674e834d7ca5ef255819357497a
es5es5 <es5e5s@nate.com>
2020-12-28 21:33:39 +0900

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello'),
-          ),
-          body: Text(
-            'Hello Flutter',
-            style: TextStyle(fontSize: 30),
-          )),
+      home: HelloPage('Hello World'),
     );
+  }
+}
+
+class HelloPage extends StatefulWidget {
+  final String title;
+
+  HelloPage(this.title): super();
+
+  @override
+  _HelloPageState createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      title: Text(widget.title)
+    ),body: Text(widget.title, style: TextStyle(fontSize: 30)));
   }
 }
